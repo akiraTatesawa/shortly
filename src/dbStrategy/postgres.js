93604834eaafe66b49f31ecf029d152f400a0c0f
Arthur Akira <arthurak10.at@gmail.com>
2022-08-03 15:47:40 -0300

    chore: setup for deploy on heroku

--- src/dbStrategy/postgres.js
+++ src/dbStrategy/postgres.js
@@ -1,11 +1,25 @@
+import "../config/config.js";
+
 import pg from "pg";
 
 const { Pool } = pg;
 
-export const connection = new Pool({
+const productionConfig = {
+  connectionString: process.env.DATABASE_URL,
+  ss1: {
+    rejectUnauthorized: false,
+  },
+};
+
+const developmentConfig = {
   user: process.env.PGUSER,
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDATABASE,
   password: process.env.PGPASSWORD,
-});
+};
+
+const databaseConfig =
+  process.env.NODE_ENV === "dev" ? developmentConfig : productionConfig;
+
+export const connection = new Pool(databaseConfig);
